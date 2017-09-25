89f7373b277cec2e5895d4552906f1d6f367bc8b
Shireen Garcia <garciasal@gmail.com>
2017-09-25 11:27:01 -0700

    Testing pull and merge

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 800,
+  width: 1000,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block'
+}
 
 class AboutUs extends Component {
     render(){
         return(
+          <div>
             <h1>AboutUs</h1>
+                <Paper style={style} zDepth={2} />
+          </div>
         )
     }
 }
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 600,
+  width: 400,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block'
+}
 
 class Contact extends Component {
     render(){
         return(
+          <div>
             <h1>Contact</h1>
+              <Paper style={style} zDepth={2} />
+          </div>
         )
     }
 }
