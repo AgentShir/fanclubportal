1b47852c0fa963d4ea7edc003a210da037a851bd
Philip <phils89144@gmail.com>
2017-09-25 10:28:25 -0700

    added /addportals api route

--- server/routes/api.js
+++ server/routes/api.js
@@ -53,6 +53,28 @@ router.post("/token", function (req, res, next) {
   }
 })
 
+router.post('/addPortals', function (req, res, next) {
+  const teamName = req.body.teamName
+  const fanClubName = req.body.fanClubName
+  const teamLocation = req.body.teamLocation
+  const fanClubLocation = req.body.fanClubLocation
+  const description = req.body.description
+
+  console.log('in router add portal', req.body)
+  if (teamName.length === 0) {
+    res.status(401).json({
+      message: 'Team Name must be entered.'
+    })
+  }else{
+    fanPortals.addFanPortal(req.body, function(success,response){
+      if(!success){
+        res.status(401).json(response)
+      }else{
+        res.json(response)
+      }
+    })
+  }
+})
 
 //EVENT ROUTES
 router.post("/:portalId/addEvent", function(req, res,next){
